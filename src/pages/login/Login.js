78f9f6ac8314275ac79f61ed88008f98fa8eccb2
OfekAvanDanan <4ofek4dj@gmail.com>
2024-06-04 18:20:26 +0300

    Log in and more

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,6 +3,7 @@ import { ThemeContext } from "../../ThemeContext.js";
 import Container from "../../components/container/Container.js";
 import GenericButton from "../../components/buttons/GenericButton.js";
 import LightButton from "../../components/buttons/LightButton.js";
+import "./login.css";
 
 const LoginForm = () => {
   const { theme } = useContext(ThemeContext);
@@ -33,7 +34,7 @@ const LoginForm = () => {
       <Container title={"Log in"} containerStyle={"login-container"}>
         <form onSubmit={handleSubmit}>
           <div className="field-container">
-            <p>Username</p>
+            <b>Username</b>
             <input
               className={`field ${theme}`}
               name="username"
@@ -42,7 +43,7 @@ const LoginForm = () => {
             />
           </div>
           <div className="field-container">
-            <p>Password</p>
+            <b>Password</b>
             <input
               className={`field ${theme}`}
               name="password"
@@ -50,6 +51,14 @@ const LoginForm = () => {
               value={formData.password}
               onChange={handleInputChange}
             />
+          </div>
+          <div className="linear-layout">
+            <div>
+              <b>Remember me</b> <input type="checkbox" value={formData.rememberMe} />
+            </div>
+            <a className={theme} href="./forgotpassword">
+              <b>I forgot my password</b>
+            </a>
           </div>
           <div className="buttons-container">
             <GenericButton text="Log in" type="submit" onClick={handleSubmit} />
