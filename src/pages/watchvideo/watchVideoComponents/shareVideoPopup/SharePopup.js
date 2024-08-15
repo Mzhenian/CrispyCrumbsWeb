b731b667fd0e6caa4500a121ba61821b4a10d6e8
Mzhenian <97063435+Mzhenian@users.noreply.github.com>
2024-08-15 23:29:56 +0300

    feat(watch video): share buttons now work

--- src/pages/watchvideo/watchVideoComponents/shareVideoPopup/SharePopup.js
+++ src/pages/watchvideo/watchVideoComponents/shareVideoPopup/SharePopup.js
@@ -9,12 +9,21 @@ import shareWhatsapp from "../../../../components/iconsLab/shareWhatsapp.svg";
 import copyIcon from "../../../../components/iconsLab/copy.svg";
 
 const SharePopup = ({ isOpen, onClose }) => {
-  const arr = [shareX, shareInstagram, shareWhatsapp, shareFacebook];
+  const url = encodeURIComponent(window.location.href);
+  const xtext = encodeURIComponent("Check this out!");
+  const shareUrlFacebook = 'https://www.facebook.com/sharer/sharer.php?u=' + url;
+  const shareUrlX = `https://twitter.com/intent/tweet?url=${url}&text=${xtext}`;
+  const shareUrlWhatsapp = `https://wa.me/?text=${url}`;
+  const InstagramUrl = "https://instagram.com/";
+  const FacebookUrl = "https://www.facebook.com/";
 
-  const handleCopyUrl = () => {
+  const handleShareLinks = (string, link) => {
     navigator.clipboard.writeText(window.location.href).then(
       () => {
-        alert("URL copied to clipboard!");
+        alert("URL copied to clipboard! " + string);
+        if (link) {
+          window.open(link, "_blank");
+        }
       },
       (err) => {
         console.error("Failed to copy: ", err);
@@ -25,12 +34,24 @@ const SharePopup = ({ isOpen, onClose }) => {
   return (
     <Popup isOpen={isOpen} onClose={onClose} title="Share this video">
       <div className="social-photos-layout">
-        {arr.map((icon) => (
-          <div className="thumbnail-container" id="social-container" key={icon}>
-            <img src={icon} alt="SocialIcon" className="social-thumbnail" />
-          </div>
-        ))}
-        <div className="thumbnail-container" id="social-container" onClick={handleCopyUrl}>
+
+        <a className="social-container" id="x-container" href={shareUrlX} target="_blank" rel="noopener noreferrer">
+          <img src={shareX} alt="share on X" className="social-thumbnail" />
+        </a>
+
+        <div className="social-container" id="facebook-container" onClick={() => handleShareLinks("continue to open Facebook.", FacebookUrl)}>
+          <img src={shareFacebook} alt="share on Instagram" className="social-thumbnail" />
+        </div>
+
+        <div className="social-container" id="instagram-container" onClick={() => handleShareLinks("continue to open Instagram.", InstagramUrl)}>
+          <img src={shareInstagram} alt="share on Instagram" className="social-thumbnail" />
+        </div>
+
+        <a className="social-container" id="whatsapp-container" href={shareUrlWhatsapp} target="_blank" rel="noopener noreferrer">
+          <img src={shareWhatsapp} alt="share on Whatsapp" className="social-thumbnail" />
+        </a>
+
+        <div className="social-container" id="share-link-container" onClick={() => handleShareLinks("")}>
           <img src={copyIcon} alt="Copy URL" className="social-thumbnail" />
         </div>
       </div>
