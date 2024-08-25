55d170b0e1264b9f943d7644cb44bb92749fc094
OfekAvanDanan <4ofek4dj@gmail.com>
2024-08-26 00:31:44 +0300

    add view to local storage

--- src/contexts/ViewContext.js
+++ src/contexts/ViewContext.js
@@ -1,9 +1,17 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const ViewContext = createContext();
 
 export const ViewProvider = ({ children }) => {
-  const [view, setView] = useState("grid");
+  const defaultView = "grid";
+
+  const [view, setView] = useState(() => {
+    return localStorage.getItem("viewMode") || defaultView;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("viewMode", view);
+  }, [view]);
 
   const toggleView = () => {
     setView((prevView) => (prevView === "grid" ? "list" : "grid"));
